6378237bcf66a6fd06cda453bbb0344e466e58e5
aslam341 <aslam.singapore@gmail.com>
2024-03-12 03:45:24 +0800

    Add ability to process transactions

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -1,5 +1,8 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ExpenseManager {
@@ -10,9 +13,69 @@ public class ExpenseManager {
     }
 
     public void addExpense(Expense expense) {
-        BigDecimal zeroValue = new BigDecimal(0);
+        BigDecimal zeroValue = new BigDecimal(0.00);
         BigDecimal currentExpense = expenses.getOrDefault(expense.getName(), zeroValue);
         BigDecimal totalExpense = currentExpense.add(expense.getCost());
         expenses.put(expense.getName(), totalExpense);
+    }
+
+    public List<String> settleExpense() {
+        List<String> transactions = new ArrayList<>();
+        List<String> borrowers = new ArrayList<>();
+        List<String> lenders = new ArrayList<>();
+
+        BigDecimal totalSumOfExpenses = calculateTotalExpenses();
+        int numberOfPeople = expenses.size();
+        BigDecimal amountForEachPerson = totalSumOfExpenses.divide(new BigDecimal(numberOfPeople),
+                2, RoundingMode.HALF_UP);
+
+        categorizeLendersAndBorrowers(lenders, borrowers, amountForEachPerson);
+
+        processTransactions(transactions, lenders, borrowers);
+
+        return transactions;
+    }
+
+    private BigDecimal calculateTotalExpenses() {
+        BigDecimal totalSumOfExpenses = BigDecimal.valueOf(0.00);
+        for (BigDecimal cost : expenses.values()) {
+            totalSumOfExpenses = totalSumOfExpenses.add(cost);
+        }
+        return totalSumOfExpenses;
+    }
+
+    private void categorizeLendersAndBorrowers(List<String> lenders,
+                                               List<String> borrowers,
+                                               BigDecimal amountForEachPerson) {
+        BigDecimal zeroValue = new BigDecimal(0.00);
+        for (Map.Entry<String, BigDecimal> entry : expenses.entrySet()) {
+            BigDecimal balanceOwed = entry.getValue().subtract(amountForEachPerson);
+            expenses.put(entry.getKey(), balanceOwed);
+            if (balanceOwed.compareTo(zeroValue) == 1) {
+                lenders.add(entry.getKey());
+            } else if (balanceOwed.compareTo(zeroValue) == -1) {
+                borrowers.add(entry.getKey());
+            }
+        }
+    }
+
+    private void processTransactions(List<String> transactions, List<String> lenders, List<String> borrowers) {
+        BigDecimal zeroValue = BigDecimal.valueOf(0.00);
+        for (String borrower : borrowers) {
+            for (String lender : lenders) {
+                BigDecimal borrowed = expenses.get(borrower).abs();
+                BigDecimal lent = expenses.get(lender);
+                BigDecimal minimumPayment = borrowed.min(lent);
+                if (borrowed.compareTo(zeroValue) == 0 || minimumPayment.compareTo(zeroValue) == 0) {
+                    continue;
+                }
+
+                expenses.put(borrower, minimumPayment.subtract(borrowed));
+                expenses.put(lender, lent.subtract(minimumPayment));
+                String transaction = borrower + " pays " + lender
+                        + " $" + minimumPayment.setScale(2, RoundingMode.HALF_UP);
+                transactions.add(transaction);
+            }
+        }
     }
 }
